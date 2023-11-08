b23dbe79af2be564a6aa8b45847da7f1a38cc19a
CACY <carloscuestay@gmail.com>
2023-11-08 01:51:34 -0500

    commit 9

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.TodoList.as_view()),
     path('todos/', views.TodoList.as_view()),
     path('todos/', views.TodoListCreate.as_view()),
     
