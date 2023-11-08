9dd12a53d2e118196fc07fd1a51b9d78178048f3
CACY <carloscuestay@gmail.com>
2023-11-08 12:36:51 -0500

    commit 18

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,15 @@ from .serializers import BackendSerializer
 from backend.models import Registro
 
 
+class TodoList(generics.ListAPIView):
+    serializer_class = BackendSerializer
+    
+    def get_queryset(self):
+        user = self.request.user
+        return Registro.objects.filter(user=user).order_by('-creado')
+    
 class TodoListCreate(generics.ListCreateAPIView):
+    serializer_class = BackendSerializer
     
     def get_queryset(self):
         user = self.request.user
