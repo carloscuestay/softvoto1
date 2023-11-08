72f565783bd40c0b0b9145f4b2c857559715a3da
CACY <carloscuestay@gmail.com>
2023-11-08 13:20:46 -0500

    commit 21

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import BackendSerializer
 from backend.models import Registro
-
+from rest_framework.permissions import IsAuthenticated
 
 class TodoList(generics.ListAPIView):
     serializer_class = BackendSerializer
-    
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
         user = self.request.user
         return Registro.objects.filter(user=user).order_by('-creado')
     
 class TodoListCreate(generics.ListCreateAPIView):
     serializer_class = BackendSerializer
+    permission_classes = [IsAuthenticated]
     
     def get_queryset(self):
         user = self.request.user
         return Registro.objects.filter(user=user).order_by('-creado')
-    
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)    
     
 
